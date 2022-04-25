e6666ca7b5b90dccaa91344d84570f757008ab73
nitrotap <kartikinpublic@gmail.com>
2022-04-24 22:00:21 -0600

    setting up server and user model

---
+++ models/User.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+
+const valEmail = function (a) {
+	const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+	return regex.test(a);
+};
+
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+			validate: [valEmail, 'Invalid Email. Please Try Again!']
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: createdAtVal => dateFormat(createdAtVal)
+		},
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Thought'
+			}
+		],
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'User'
+			}
+		]
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		},
+		// prevents virtuals from creating duplicate of _id as `id`
+		id: false
+	}
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function () {
+	return this.friends.length;
+});
+
+const Pizza = model('Pizza', PizzaSchema);
+
+module.exports = Pizza;
